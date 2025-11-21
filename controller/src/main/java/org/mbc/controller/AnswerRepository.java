20f65851069deebfc459e63a2b61f302de5ffbdb
ksh8449 <ziran8449@gmail.com>
2025-11-21 10:21:33 +0900

    컨트롤 커밋

---
+++ controller/src/main/java/org/mbc/controller/AnswerRepository.java
@@ -0,0 +1,6 @@
+package org.mbc.controller;
+
+import org.springframework.data.jpa.repository.JpaRepository;
+
+public interface AnswerRepository  extends JpaRepository<Answer, Integer> {
+}
---
+++ controller/src/test/java/org/mbc/controller/ControllerApplicationTests.java
@@ -0,0 +1,22 @@
+package org.mbc.controller;
+
+import static org.junit.jupiter.api.Assertions.assertEquals;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ControllerApplicationTests {
+
+	@Autowired
+	private QuestionRepository questionRepository;
+
+	@Test
+	void testJpa() {
+		Question q = this.questionRepository.findBySubjectAndContent(
+				"sbb가 무엇인가요?", "sbb에 대해서 알고 싶습니다.");
+		assertEquals(1, q.getId());
+	}
+
+}
